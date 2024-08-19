eca4554d04964340d99df650cfed0413fd52fc5a
ThePhaseCat <alexmgamer10@gmail.com>
2024-08-19 16:38:09 -0600

    added a test command

--- src/client/java/phase/distance_travel/DistanceTravelClient.java
+++ src/client/java/phase/distance_travel/DistanceTravelClient.java
@@ -1,6 +1,9 @@
 package phase.distance_travel;
 
+import static net.minecraft.server.command.CommandManager.*;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +14,13 @@ public class DistanceTravelClient implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("foo")
+				.executes(context -> {
+					context.getSource().sendFeedback(() -> Text.literal("you just called this command"), false);
+					return 1;
+				})));
+
 		LOGGER.info("Distance Travel client initialized!");
 	}
 }
