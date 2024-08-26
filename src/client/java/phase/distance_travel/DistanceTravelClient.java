15babb80b37bbe5500c1f2c66d3a2db3b1b414fe
ThePhaseCat <alexmgamer10@gmail.com>
2024-08-25 19:09:18 -0600

    reformating

--- src/client/java/phase/distance_travel/DistanceTravelClient.java
+++ src/client/java/phase/distance_travel/DistanceTravelClient.java
@@ -1,12 +1,11 @@
 package phase.distance_travel;
 
-import static net.minecraft.server.command.CommandManager.*;
+import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +20,8 @@ public class DistanceTravelClient implements ClientModInitializer {
 	Timer timer = new Timer();
 
 	public static boolean isDistanceTravelModeOn = false;
+
+	public static boolean isTimerActive = false;
 
 	public static int startXPosition = 0;
 
@@ -39,21 +40,19 @@ public class DistanceTravelClient implements ClientModInitializer {
 
 		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal("dt_start")
 				.executes(context -> {
-					startXPosition = MinecraftClient.getInstance().player.getBlockPos().getX();
-					startZPosition = MinecraftClient.getInstance().player.getBlockPos().getZ();
-					LOGGER.info("Start position set to: " + startXPosition + ", " + startZPosition);
-					isDistanceTravelModeOn = true;
+					start_DT_track(context);
 					return 1;
 				})));
 
 		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal("dt_end")
 				.executes(context -> {
-					int endXPosition = MinecraftClient.getInstance().player.getBlockPos().getX();
-					int endZPosition = MinecraftClient.getInstance().player.getBlockPos().getZ();
-					LOGGER.info("End position set to: " + endXPosition + ", " + endZPosition);
-					int distance = Math.abs(endXPosition - startXPosition);
-					context.getSource().sendFeedback(Text.of("Distance Travelled: " + distance));
-					isDistanceTravelModeOn = false;
+					end_DT_track(context);
+					return 1;
+				})));
+
+		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal("dt_stats")
+				.executes(context -> {
+					DT_stats(context);
 					return 1;
 				})));
 
@@ -67,5 +66,52 @@ public class DistanceTravelClient implements ClientModInitializer {
 		}, 5000, 5000);
 
 		LOGGER.info("Distance Travel client finished starting! Have fun with all your epic calculations!");
+	}
+
+	public void start_DT_track(CommandContext<FabricClientCommandSource> context)
+	{
+		startXPosition = MinecraftClient.getInstance().player.getBlockPos().getX();
+		startZPosition = MinecraftClient.getInstance().player.getBlockPos().getZ();
+		LOGGER.info("Start position set to: " + startXPosition + ", " + startZPosition);
+		isDistanceTravelModeOn = true;
+	}
+
+	public void end_DT_track(CommandContext<FabricClientCommandSource> context)
+	{
+		int endXPosition = MinecraftClient.getInstance().player.getBlockPos().getX();
+		int endZPosition = MinecraftClient.getInstance().player.getBlockPos().getZ();
+		LOGGER.info("End position set to: " + endXPosition + ", " + endZPosition);
+		int distance = Math.abs(endXPosition - startXPosition);
+		//context.getSource().sendFeedback(Text.of("Distance Travelled: " + distance));
+		isDistanceTravelModeOn = false;
+	}
+
+	public void DT_stats(CommandContext<FabricClientCommandSource> context)
+	{
+		if(isTimerActive)
+		{
+			context.getSource().sendFeedback(Text.of("Please wait. Wrapping tracking up."));
+		}
+		if(isDistanceTravelModeOn)
+		{
+			context.getSource().sendFeedback(Text.of("Distance Travel Mode is currently on. Please use /dt_end to end tracking."));
+		}
+		else
+		{
+			context.getSource().sendFeedback(Text.of("Distance Travel Mode is currently off. Please use /dt_start to begin tracking."));
+		}
+
+	}
+
+	public void timerStuff()
+	{
+		if(isDistanceTravelModeOn)
+		{
+			//add stuff here
+		}
+		else
+		{
+			//run logic to end it
+		}
 	}
 }
